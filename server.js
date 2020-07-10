3a67f7bdf10a0c4e44dd232847993a14fedba6f7
siwel20 <plthrock@gmail.com>
2020-07-09 22:37:32 -0600

    created routes to serve other pages

--- server.js
+++ server.js
@@ -110,6 +110,14 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
+app.get('/animals', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/animals.html'));
+});
+
+app.get('/zookeepers', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+});
+
 app.post('/api/animals', (req, res) => {
     // req.body is where our incoming content will be
     //console.log(req.body);
