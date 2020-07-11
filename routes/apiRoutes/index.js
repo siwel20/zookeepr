55480b1afe52bce6a2842dcf86cca725c1d5636f
siwel20 <plthrock@gmail.com>
2020-07-11 11:03:56 -0600

    built dataset and functionality for zookeepers

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const animalRoutes = require('../apiRoutes/animalRoutes');
+const zookeepers = require('../../lib/zookeepers');
 
+router.use(require('./zookeeperRoutes'));
 router.use(animalRoutes);
 
 module.exports = router;
