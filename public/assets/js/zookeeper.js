2dd1f430f3caa7e398dbb3d9ec7f7ac7c5da8e01
siwel20 <plthrock@gmail.com>
2020-07-11 12:28:54 -0600

    created zookeeper front-end functionality

--- public/assets/js/zookeeper.js
+++ public/assets/js/zookeeper.js
@@ -1,9 +1,16 @@
+const $zookeeperForm = document.querySelector("#zookeeper-form");
 const $displayArea = document.querySelector('#display-area');
+
 
 const printResults = resultArr => {
   console.log(resultArr);
 
-  const animalHTML = resultArr.map(({ id, name, age, favoriteAnimal }) => {
+  const animalHTML = resultArr.map(({
+    id,
+    name,
+    age,
+    favoriteAnimal
+  }) => {
     return `
   <div class="col-12 col-md-5 mb-3">
     <div class="card p-3" data-id=${id}>
@@ -20,11 +27,17 @@ const printResults = resultArr => {
   $displayArea.innerHTML = animalHTML.join('');
 };
 
-const getZookeepers = () => {
-  fetch('/api/zookeepers')
+const getZookeepers = (formData = {}) => {
+  let queryUrl = '/api/zookeepers?';
+
+  Object.entries(formData).forEach(([key, value]) => {
+    queryUrl += `${key}=${value}&`;
+  });
+
+  fetch(queryUrl)
     .then(response => {
       if (!response.ok) {
-        return alert('Error: ' + response.statusText);
+        return alert(`Error: ${response.statusText}`);
       }
       return response.json();
     })
@@ -33,5 +46,24 @@ const getZookeepers = () => {
       printResults(zookeeperArr);
     });
 };
+
+const handleGetZookeepersSubmit = event => {
+  event.preventDefault();
+  const nameHTML = $zookeeperForm.querySelector('[name="name"]');
+  const name = nameHTML.value;
+
+  const ageHTML = $zookeeperForm.querySelector('[name="age"]');
+  const age = ageHTML.value;
+
+  const zookeeperObject = {
+    name,
+    age
+  };
+
+  getZookeepers(zookeeperObject);
+};
+
+$zookeeperForm.addEventListener('submit', handleGetZookeepersSubmit);
+
 
 getZookeepers();
